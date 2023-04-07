47417369519c2558cea7bf5edc5e117a80bd5198
Matheus Bueno Nardi <matheusnardi2004@gmail.com>
2023-04-06 21:39:21 -0300

    js e html

---
+++ Curso/Estruturas/projeto/js/script.js
@@ -0,0 +1,9 @@
+function alterarH1(msg) {
+    document.getElementById("msg").innerHTML = msg;
+}
+
+   
+function alterarCorTamanho() {
+    document.getElementById("msg").style.color = "blue";
+    document.getElementById('msg').style.fontSize = "100px"
+}
