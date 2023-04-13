e5ba42cdca4aceee512df83dc8ac52306f7e17e8
Matheus Bueno Nardi <matheusnardi2004@gmail.com>
2023-04-12 21:53:24 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function alterarH1(msg) {
+    document.getElementById("msg").innerHTML = msg;
+}
+
+   
+function alterarCorTamanho() {
+    document.getElementById("msg").style.color = "#483D8B";
+    document.getElementById('msg').style.fontSize = "100px"
+}
